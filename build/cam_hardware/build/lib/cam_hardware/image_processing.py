69bf125f4f15ded6d182bcbf669c94442efcc93f
Chinedu <chinyony@gmail.com>
2023-10-26 13:30:31 -0700

    updating image processor and adding motor control

--- build/cam_hardware/build/lib/cam_hardware/image_processing.py
+++ build/cam_hardware/build/lib/cam_hardware/image_processing.py
@@ -16,12 +16,26 @@ class ImageProcessing(Node):
         super().__init__("image_processing")
         self.bridge = CvBridge()
         self.sub = self.create_subscription(Image, "/image", self.listener_callback,10)
-        self.model = YOLO("runs/detect/train9/weights/best.onnx")
-    
+        self.model = YOLO("yolo_training/runs/detect/train/weights/best.onnx")
+        self.middle_range = [300,340]
+
     def listener_callback(self, msg):
         try:
             img_msg = self.bridge.imgmsg_to_cv2(msg)
-            results = self.model.predict(source=img_msg, show=True)
+            results = self.model.predict(source=img_msg, show=True, classes=16, conf=0.6, verbose= False)
+            for r in results:
+                boxes = r.boxes.xyxy
+                for box in boxes:
+                    middle_detection = (box[0] + box[2])/2
+                    if min(self.middle_range) < middle_detection < max(self.middle_range):
+                        self.get_logger().info("YOURE IN THE MIDDLE")
+                    elif min(self.middle_range) > middle_detection:
+                        self.get_logger().info("MOVE LEFT")
+                    elif max(self.middle_range) < middle_detection:
+                        self.get_logger().info("MOVE RIGHT")
+
+                        
+                
 
         except CvBridgeError as e:
             print(f"THIS IS ERROR: {e}")
